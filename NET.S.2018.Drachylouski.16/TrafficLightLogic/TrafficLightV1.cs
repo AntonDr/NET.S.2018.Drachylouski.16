366db688fd4cd280295d1e1c12e5e0cda77a7121
AntonDr <31773079+AntonDr@users.noreply.github.com>
2018-07-26 20:19:37 +0300

    Trafficlight task

---
+++ NET.S.2018.Drachylouski.16/TrafficLightLogic/TrafficLightV1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLightLogic
+{
+    public class TrafficLightV1<T>
+    {
+        private Queue<T> queue;
+
+        public TrafficLightV1(IEnumerable<T> collection)
+        {
+            queue=new Queue<T>(collection);
+        }
+
+        public T Current { get; private set ; }
+
+        public void Update()
+        {
+            Current = queue.Dequeue();
+                
+            queue.Enqueue(Current);
+        }
+
+        public IEnumerable<T> Iteration()
+        {
+            int k = queue.Count;
+
+            IEnumerable<T> InsideIteration()
+            {
+                while (k != 0)
+                {
+                    Update();
+                    yield return Current;
+                    k--;
+                }
+                Update();
+                yield return Current;
+            }
+
+            return InsideIteration();
+        }
+
+    }
+}
---
+++ NET.S.2018.Drachylouski.16/TrafficLightLogic/TrafficLightV2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLightLogic
+{
+    public class TrafficLightV2
+    {
+       
+        public ITrafficLightState State { get; set; }
+
+        public TrafficLightV2(ITrafficLightState trafficLightState)
+        {
+            State = trafficLightState;
+        }
+
+        public void CurrentColor()
+        {
+            State.CurrentColor(this);
+        }
+
+        public void NextColor()
+        {
+            State.NextColor(this);
+        }
+
+        public void PriviousColor()
+        {
+            State.PreviousColor(this);
+        }
+    }
+
+    public interface ITrafficLightState
+    {
+        void CurrentColor(TrafficLightV2 trafficLight);
+        void NextColor(TrafficLightV2 trafficLight);
+        void PreviousColor(TrafficLightV2 trafficLight);
+    }
+
+    public class GreenTrafficLightColor : ITrafficLightState
+    {
+        public void CurrentColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Green!");
+        }
+
+        public void NextColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Yellow!");
+            trafficLight.State = new YellowAfterGreenTrafficLightColor();
+        }
+
+
+
+        public void PreviousColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Yellow!");
+            trafficLight.State = new YellowAfterGreenTrafficLightColor();
+        }
+    }
+
+    public class YellowAfterRedTrafficLightColor : ITrafficLightState
+    {
+        public void CurrentColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Yellow!");
+        }
+
+        public void NextColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Green!");
+            trafficLight.State = new GreenTrafficLightColor();
+        }
+
+        public void PreviousColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Red!");
+            trafficLight.State = new RedTrafficLightColor();
+        }
+    }
+
+    public class YellowAfterGreenTrafficLightColor : ITrafficLightState
+    {
+        public void CurrentColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Yellow!");
+        }
+
+        public void NextColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Red");
+            trafficLight.State = new RedTrafficLightColor();
+        }
+
+        public void PreviousColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Green!");
+            trafficLight.State = new GreenTrafficLightColor();
+
+        }
+    }
+
+    public class RedTrafficLightColor : ITrafficLightState
+    {
+        public void CurrentColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Red!");
+        }
+
+        public void NextColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Yellow!");
+            trafficLight.State = new YellowAfterRedTrafficLightColor();
+        }
+
+        public void PreviousColor(TrafficLightV2 trafficLight)
+        {
+            Console.WriteLine("Yellow");
+            trafficLight.State = new YellowAfterGreenTrafficLightColor();
+        }
+    }
+
+}
